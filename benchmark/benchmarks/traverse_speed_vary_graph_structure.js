9b1b719f879ff751037190d5415ef0ff54c50afb
Mathias Bak <bufas7@gmail.com>
2015-05-28 23:24:21 +0200

    Rewrite traverse speed benchmark

--- benchmark/benchmarks/traverse_speed_vary_graph_structure.js
+++ benchmark/benchmarks/traverse_speed_vary_graph_structure.js
@@ -11,91 +11,73 @@
  * 
  */
 
-var traverseSpeedVaryGraphStructure = function () {
+var traverseSpeedVaryGraphStructure = {
 
-  var dataDirectory = 'traverse_speed_vary_graph_structure';
+  dataDirectory : 'data/traverse_speed_vary_graph_structure/',
 
-  // Bench matrix
-  var nodes = [
-    1000,
-    900, 
-    800, 
-    700, 
-    600, 
-    500, 
-    400, 
-    300, 
-    200, 
-    100, 
-    30, 
-    10
-  ];
-  var time = [
-    1000
-  ];
-  var configurations = {
-    'flat'  : graphConfigurations.flat(),
-    'snake' : graphConfigurations.nesting(),
-    '2ary'  : graphConfigurations.nary(2),
-    '4ary'  : graphConfigurations.nary(4)
-  };
+  header : ['# iter n'],
 
-  // Instantiate Library Adapter and zip file
-  var lalConfig = { shareConf: shareConfigurations.default };
-  var lal = new ThreeAdapter(lalConfig);
-  var zip = new JSZip();
+  globalSetup : function () {
+    var lalConfig = { shareConf: shareConfigurations.default };
+    this.lal = new ThreeAdapter(lalConfig);
+  },
 
-  // Benchmark
-  Object.keys(configurations).forEach(function (conf) {
-    var data = [['#', 'iter', 'n', 'time']];
+  fileSetup     : function (args) {
 
-    time.forEach(function (t) {
-      nodes.forEach(function (n) {
-        var scene = new SceneGraph(configurations[conf](n), factories.default);
-        var iter  = timeCalcDiffModel(lal, scene, t);
-        data.push([iter, n, t]);
+  },
+
+  testSetup : function (args) {
+    this.scene = new SceneGraph(args.conf(args.nodes), factories.default);
+  },
+
+  testCase : function () {
+    this.lal.calculateDiffModel(this.scene.root);
+  },
+
+  testMatrix : (function () {
+    // Bench matrix
+    var nodes = [
+      1000,
+      900, 
+      800, 
+      700, 
+      600, 
+      500, 
+      400, 
+      300, 
+      200, 
+      100, 
+      30, 
+      10
+    ];
+    var configurations = {
+      'flat'  : graphConfigurations.flat(),
+      'snake' : graphConfigurations.nesting(),
+      '2ary'  : graphConfigurations.nary(2),
+      '4ary'  : graphConfigurations.nary(4)
+    };
+
+    // Benchmark
+    var testMatrix = [];
+    Object.keys(configurations).forEach(function (confName) {
+      testMatrix.push({
+        filename : confName + '.dat',
+        fileData : {confName: confName, conf: configurations[confName]},
+        lineData : nodes.map(function (n) {
+          return {nodes: n};
+        })
       });
     });
 
-    var dataString = data.map(function (row) { return row.join(' '); }).join('\n');
-    zip.file('data/traverse_speed_vary_graph_structure/'+conf+'.dat', dataString);
-  });
+    return testMatrix;
+  })(),
 
-  // Download the file
-  location.href="data:application/zip;base64,"+zip.generate();
-
-}
-
-/**
- * Measures the time it takes to calculate the diff model on a scene graph a
- * number of times
- * @param  {THREE.Scene} the root of the scene graph
- * @param {number} iterations - the number of times to calculate the diff model
- * @return {number} The number of miliseconds to run the test
- */
-var timeCalcDiffModel = function (lal, scene, ms) {
-  var start = performance.now();
-  var end;
-  var iterations = 0;
-  var dm = {}; var cnt = 0; // Force side effects
-  while (true) {
-    dm = lal.calculateDiffModel(scene.root);
-    iterations++;
-
-    // Force side effects
-    if (dm) cnt++;
-
-    end = performance.now();
-    if (end - start > ms) break;
+  makeLine : function (args) {
+    return [args.res, args.nodes];
   }
-
-  console.log(cnt);
-  return iterations / ((end - start) / 1000);
 }
 
 $(function () {
-  var benchBtn = $('<button type="button">');
-  benchBtn.text('traverse speed vary graph structure');
-  benchBtn.on('click', traverseSpeedVaryGraphStructure);
-  $('.benchmark-buttons').append(benchBtn);
+  var name = 'traverse speed vary graph structure';
+  benchmarker.addBench(name, traverseSpeedVaryGraphStructure);
 });
